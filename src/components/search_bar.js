7458a0bf5f9f9b41c39b1c631119fbe4b5419515
harisonde <harisonde@yahoo.co.in>
2018-03-31 11:37:48 -0500

    Added changes for Video_list and item

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,21 +1,29 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component{
+
+constructor(props){
+  super(props);
+
+  this.state = {
+      term: ''
+  };
+}
+
 render(){
       return(
-          <div>
+      <div>
           <br/><br/>
           Enter your search criteria:
-          <input type='text' name= 'searchBar'
-          onChange={this.onInputChange}/>
-          </div>
+          <input type='text'
+            value = {this.state.term}
+            onChange={event  => this.setState({...this.state, term: event.target.value})}/>
+        </div>
         );
   }
 
 onInputChange(event){
-console.log(event.target.value);
 }
-
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import React from '../node_modules/react';
+import React, {Component} from '../node_modules/react';
 import ReactDom from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+
 const YOU_TUBE_API_KEY = 'AIzaSyD0zBK2ufruZfgzP45YEFVwyvqI-7sgUIM';
 
-const App = () => {
-return <div>
-  <SearchBar/>
-</div>;
-};
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      videos : []
+    };
+
+    YTSearch({key: YOU_TUBE_API_KEY, term:'cricket'}, (videos) => {
+        this.setState({videos});
+    });
+  }
+
+    render(){
+      return(
+        <div>
+            <SearchBar/>
+            <VideoList videos = {this.state.videos}/>
+        </div>
+      );
+    }
+}
 
 ReactDom.render(<App />, document.querySelector('.container') );
