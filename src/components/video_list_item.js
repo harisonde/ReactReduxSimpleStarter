6b35fabcc0296a4cf2c5159ea2616af14eaacccd
harisonde <harisonde@yahoo.co.in>
2018-03-31 20:50:54 -0500

    changes to video list item

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
 const VideoListItem = (props) => {
+  const {video} = props;
+  console.log(video);
+  const imgUrl = video.snippet.thumbnails.default.url;
   return (
-    <li>Video</li>
+    <li className="list-group-item">
+      <div className="video-list-item">
+
+        <div className="media-left">
+          <img className="media-object" src={imgUrl}/>
+        </div>
+
+        <div className="media-body">
+            <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
   );
 }
 
